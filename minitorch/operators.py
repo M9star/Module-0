ff17813a93a0525de5d9a936811a24806f587a37
Mausam Gurung <mausaam.gurung593@gmail.com>
2026-09-25 08:38:24 +0545

    soutlion of task 0.3

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -138,74 +138,85 @@ def relu_back(x: float, d: float) -> float:
 # TODO: Implement for Task 0.3.
 
 
-def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
+def map(fn):
     """
     Higher-order map.
 
-    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
+    .. image:: figs/Ops/maplist.png
+
+
+    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
 
     Args:
-        fn: Function from one value to one value.
+        fn (one-arg function): Function from one value to one value.
 
     Returns:
-        A function that takes a list, applies `fn` to each element, and returns a
-         new list
+        function : A function that takes a list, applies `fn` to each element, and returns a
+        new list
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    return lambda list: [fn(x) for x in list]
 
 
-def negList(ls: Iterable[float]) -> Iterable[float]:
-    "Use `map` and `neg` to negate each element in `ls`"
-    raise NotImplementedError("Need to include this file from past assignment.")
+def negList(ls):
+    "Use :func:`map` and :func:`neg` to negate each element in `ls`"
+    return map(neg)(ls)
 
 
-def zipWith(
-    fn: Callable[[float, float], float]
-) -> Callable[[Iterable[float], Iterable[float]], Iterable[float]]:
+def zipWith(fn):
     """
     Higher-order zipwith (or map2).
 
-    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
+    .. image:: figs/Ops/ziplist.png
+
+    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
 
     Args:
-        fn: combine two values
+        fn (two-arg function): combine two values
 
     Returns:
-        Function that takes two equally sized lists `ls1` and `ls2`, produce a new list by
-         applying fn(x, y) on each pair of elements.
+        function : takes two equally sized lists `ls1` and `ls2`, produce a new list by
+        applying fn(x, y) on each pair of elements.
 
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    return lambda ls1, ls2: (fn(x, y) for x, y in zip(ls1, ls2))
 
 
-def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
-    "Add the elements of `ls1` and `ls2` using `zipWith` and `add`"
-    raise NotImplementedError("Need to include this file from past assignment.")
+def addLists(ls1, ls2):
+    "Add the elements of `ls1` and `ls2` using :func:`zipWith` and :func:`add`"
+    return zipWith(add)(ls1, ls2)
 
 
-def reduce(
-    fn: Callable[[float, float], float], start: float
-) -> Callable[[Iterable[float]], float]:
+def reduce(fn, start):
     r"""
     Higher-order reduce.
 
+    .. image:: figs/Ops/reducelist.png
+
+
     Args:
-        fn: combine two values
-        start: start value $x_0$
+        fn (two-arg function): combine two values
+        start (float): start value :math:`x_0`
 
     Returns:
-        Function that takes a list `ls` of elements
-         $x_1 \ldots x_n$ and computes the reduction :math:`fn(x_3, fn(x_2,
-         fn(x_1, x_0)))`
+        function : function that takes a list `ls` of elements
+        :math:`x_1 \ldots x_n` and computes the reduction :math:`fn(x_3, fn(x_2,
+        fn(x_1, x_0)))`
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+
+    def _reduce(ls, fn, start):
+        iterator = iter(ls)
+        for i in iterator:
+            start = fn(start, i)
+        return start
+
+    return lambda ls: _reduce(ls, fn, start)
 
 
-def sum(ls: Iterable[float]) -> float:
-    "Sum up a list using `reduce` and `add`."
-    raise NotImplementedError("Need to include this file from past assignment.")
+def sum(ls):
+    "Sum up a list using :func:`reduce` and :func:`add`."
+    return reduce(add, 0)(ls)
 
 
-def prod(ls: Iterable[float]) -> float:
-    "Product of a list using `reduce` and `mul`."
-    raise NotImplementedError("Need to include this file from past assignment.")
+def prod(ls):
+    "Product of a list using :func:`reduce` and :func:`mul`."
+    return reduce(mul, 1)(ls)
