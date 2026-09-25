7f94ad18baef44270d78d4af00f27c57bc810ffc
Mausam Gurung <mausaam.gurung593@gmail.com>
2026-09-25 08:33:42 +0545

    done writing  high-order fucntion

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -134,4 +134,78 @@ def relu_back(x: float, d: float) -> float:
 # - prod: take the product of lists
 
 
+
 # TODO: Implement for Task 0.3.
+
+
+def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
+    """
+    Higher-order map.
+
+    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
+
+    Args:
+        fn: Function from one value to one value.
+
+    Returns:
+        A function that takes a list, applies `fn` to each element, and returns a
+         new list
+    """
+    raise NotImplementedError("Need to include this file from past assignment.")
+
+
+def negList(ls: Iterable[float]) -> Iterable[float]:
+    "Use `map` and `neg` to negate each element in `ls`"
+    raise NotImplementedError("Need to include this file from past assignment.")
+
+
+def zipWith(
+    fn: Callable[[float, float], float]
+) -> Callable[[Iterable[float], Iterable[float]], Iterable[float]]:
+    """
+    Higher-order zipwith (or map2).
+
+    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
+
+    Args:
+        fn: combine two values
+
+    Returns:
+        Function that takes two equally sized lists `ls1` and `ls2`, produce a new list by
+         applying fn(x, y) on each pair of elements.
+
+    """
+    raise NotImplementedError("Need to include this file from past assignment.")
+
+
+def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
+    "Add the elements of `ls1` and `ls2` using `zipWith` and `add`"
+    raise NotImplementedError("Need to include this file from past assignment.")
+
+
+def reduce(
+    fn: Callable[[float, float], float], start: float
+) -> Callable[[Iterable[float]], float]:
+    r"""
+    Higher-order reduce.
+
+    Args:
+        fn: combine two values
+        start: start value $x_0$
+
+    Returns:
+        Function that takes a list `ls` of elements
+         $x_1 \ldots x_n$ and computes the reduction :math:`fn(x_3, fn(x_2,
+         fn(x_1, x_0)))`
+    """
+    raise NotImplementedError("Need to include this file from past assignment.")
+
+
+def sum(ls: Iterable[float]) -> float:
+    "Sum up a list using `reduce` and `add`."
+    raise NotImplementedError("Need to include this file from past assignment.")
+
+
+def prod(ls: Iterable[float]) -> float:
+    "Product of a list using `reduce` and `mul`."
+    raise NotImplementedError("Need to include this file from past assignment.")
